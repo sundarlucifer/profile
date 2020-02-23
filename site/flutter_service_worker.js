e9f0e809bbc96fbd835c1a8fc13df39c47eefda7
sundarlucifer <sundarlucifer@gmail.com>
2020-02-23 14:25:51 +0530

    Updated hosting

--- site/flutter_service_worker.js
+++ site/flutter_service_worker.js
@@ -2,12 +2,12 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "8235cf22d337e7091e683b71fee9333e",
-"/main.dart.js": "9124d7f0755f939bf6ed5b10acddcc9e",
+"/main.dart.js": "2cad4f966601a96ed953e8217072284d",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "6acd06a3e46f7f2804d7a1d155a11a09",
-"/assets/LICENSE": "d5e242e0748806a5f7eb2065c5bbc176",
+"/assets/LICENSE": "1a80be6c5724a31e6f9c9e06dba53b63",
 "/assets/AssetManifest.json": "4792523b5a548b6215c9408c59c90e59",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
